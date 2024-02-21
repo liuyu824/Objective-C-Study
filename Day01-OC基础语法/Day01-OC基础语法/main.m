b344e4e05a0ef7e89f72b8a1daee862d07208c71
liuyu98824 <liuyu11@baicgroup.com.cn>
2024-02-21 14:16:11 +0800

    update

---
+++ Day01-OC基础语法/Day01-OC基础语法/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Day01-OC基础语法
+//
+//  Created by 刘宇 on 2023/11/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, Liuyu!");
+    }
+    return 0;
+}
---
+++ Day02-类与对象/Day02-类与对象/main.m
@@ -0,0 +1,37 @@
+//
+//  main.m
+//  Day02-类与对象
+//
+//  Created by 刘宇 on 2024/2/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iPhone : NSObject
+{
+    @public
+    NSString *_phoneNum;
+}
+
+-(void)callWithNumber;
+
+@end
+
+@implementation iPhone
+-(void)callWithNumber{
+    NSLog(@"I make a phone call by %@" , _phoneNum);
+}
+
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        iPhone *iPhone15pm = [iPhone new];
+        iPhone15pm->_phoneNum = @"13611244680";
+        [iPhone15pm callWithNumber];
+    }
+    return 0;
+}
